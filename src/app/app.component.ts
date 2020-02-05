bb3328356d56807c71af331f2cea74a03beb1b32
Oleg <w.shevchuk.o@gmail.com>
2020-02-05 12:01:06 +0200

    Base HTML, fonts added, chat data is set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from './interfaces/messegeInterface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Antagosoft-test';
+  messages: Array<Message> = [
+    {
+      author: 'Самуил',
+      date: '13 октября',
+      messageText: 'Привет, Верунь! ниче себе ты крутая. фотка класс!!!! '
+    },
+    {
+      author: 'Лилия Семёновна  ',
+      date: '14 октября 2011',
+      messageText: 'Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?'
+    },
+    {
+      author: 'Лилия Семёновна',
+      date: '14 октября 2011',
+      messageText: 'Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?'
+    }
+  ];
 }
