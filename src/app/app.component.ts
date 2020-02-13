6b173a977482f56a4adaebe0627b4ff45c480a78
Oleg <w.shevchuk.o@gmail.com>
2020-02-13 16:54:30 +0200

    Pre Angular update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit} from '@angular/core';
 import { Message } from './interfaces/messegeInterface';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+
   messages: Array<Message> = [
     {
       author: 'Самуил',
-      date: '13 октября',
+      date: '13 октября 2011',
       messageText: 'Привет, Верунь! ниче себе ты крутая. фотка класс!!!! '
     },
     {
@@ -24,4 +27,24 @@ export class AppComponent {
       messageText: 'Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?'
     }
   ];
+
+  // Reactive form declaration
+  reactiveForm: FormGroup;
+
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      message: new FormControl('', [Validators.required])
+    });
+  }
+
+  onMessageSending() {
+    const curentTime = new Date();
+    const newMessage: Message = {
+      author: 'Your profile name',
+      date: curentTime.toLocaleString('ru', {year: 'numeric', month: 'long', day: 'numeric'}).slice(0, -3),
+      messageText: `${this.reactiveForm.get('message').value}`
+    };
+    this.messages.push(newMessage);
+    this.reactiveForm.reset();
+  }
 }
